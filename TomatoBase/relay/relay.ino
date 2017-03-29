649feac639334d9e77ac699f451a9617cd82d354
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-29 19:56:12 +0900

    Refactor code

--- TomatoBase/relay/relay.ino
+++ TomatoBase/relay/relay.ino
@@ -1,30 +1,15 @@
 #define STOP_PIN 3
 #define LOW_PIN 9
 #define TURBO_PIN 10
-#define STEP 200
 
-int LowPower(){
-  digitalWrite(LOW_PIN, HIGH);
-  delay(STEP);
-  digitalWrite(LOW_PIN, LOW);
-  delay(STEP);
-}
+int LowPower();
+int Turbo();
+void Stop();
 
-int Turbo(){
-  digitalWrite(TURBO_PIN, HIGH);
-  delay(STEP);
-  digitalWrite(TURBO_PIN, LOW);
-  delay(STEP);
-}
+void switch_do(int);
 
-void Stop(){
-  digitalWrite(STOP_PIN, HIGH);
-  delay(STEP);
-  digitalWrite(STOP_PIN, LOW);
-  delay(STEP);
-}
-
-void setup(){
+void setup()
+{
   pinMode(STOP_PIN,  OUTPUT);
   pinMode(LOW_PIN,   OUTPUT);
   pinMode(TURBO_PIN, OUTPUT);
@@ -32,15 +17,40 @@ void setup(){
   Serial.begin(9600);
 }
 
-void loop(){
-  char i;
-
-  if(Serial.available()){
-    i = Serial.read();
-    switch(i){
+void loop()
+{
+  if (Serial.available())
+  {
+    char ch = Serial.read();
+    switch (ch)
+    {
     case 0: Stop(); break;
     case 1: LowPower(); break;
     case 2: Turbo(); break;
     }
   }
+}
+
+int LowPower()
+{
+  switch_do(LOW_PIN);
+}
+
+int Turbo()
+{
+  switch_do(TURBO_PIN);
+}
+
+void Stop()
+{
+  switch_do(STOP_PIN);
+}
+
+void switch_do(int pin_num)
+{
+  const int step = 200;
+  digitalWrite(pin_num, HIGH);
+  delay(step);
+  digitalWrite(pin_num, LOW);
+  delay(step);
 }
