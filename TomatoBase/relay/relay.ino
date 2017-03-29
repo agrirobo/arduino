0bf46487561ff4c08777d3cf484f30853c71d55d
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-29 19:47:48 +0900

    Reformat code

--- TomatoBase/relay/relay.ino
+++ TomatoBase/relay/relay.ino
@@ -3,35 +3,30 @@
 #define TURBO_PIN 10
 #define STEP 200
 
-
-
 int LowPower(){
-  digitalWrite(LOW_PIN,HIGH);
+  digitalWrite(LOW_PIN, HIGH);
   delay(STEP);
-  digitalWrite(LOW_PIN,LOW);
+  digitalWrite(LOW_PIN, LOW);
   delay(STEP);
 }
 
-
 int Turbo(){
-  digitalWrite(TURBO_PIN,HIGH);
+  digitalWrite(TURBO_PIN, HIGH);
   delay(STEP);
-  digitalWrite(TURBO_PIN,LOW);
+  digitalWrite(TURBO_PIN, LOW);
   delay(STEP);
 }
 
 void Stop(){
-  digitalWrite(STOP_PIN,HIGH);
+  digitalWrite(STOP_PIN, HIGH);
   delay(STEP);
-  digitalWrite(STOP_PIN,LOW);
+  digitalWrite(STOP_PIN, LOW);
   delay(STEP);
 }
 
-
-
 void setup(){
-  pinMode(STOP_PIN, OUTPUT);
-  pinMode(LOW_PIN, OUTPUT);
+  pinMode(STOP_PIN,  OUTPUT);
+  pinMode(LOW_PIN,   OUTPUT);
   pinMode(TURBO_PIN, OUTPUT);
 
   Serial.begin(9600);
@@ -41,15 +36,11 @@ void loop(){
   char i;
 
   if(Serial.available()){
-  i = Serial.read();
-  switch(i){
-  case 0: Stop(); break;
-  case 1: LowPower(); break;
-  case 2: Turbo(); break;
+    i = Serial.read();
+    switch(i){
+    case 0: Stop(); break;
+    case 1: LowPower(); break;
+    case 2: Turbo(); break;
+    }
   }
-  }
-
 }
-
-
-
